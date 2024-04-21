17044f90259b0012db573808f44c36380cf2e781
Jonas <dogdoberis@gmail.com>
2024-04-21 15:14:16 +0300

    qr code generator user error handling

---
+++ src/main/java/lt/jono/qr_gen/Main.java
@@ -0,0 +1,7 @@
+package lt.jono.qr_gen;
+
+public class Main {
+    public static void main(String[] args) {
+        QRCodeCenerator.launch(QRCodeCenerator.class, args);
+    }
+}
--- src/main/java/lt/jono/qr_gen/controller/QRGenController.java
+++ src/main/java/lt/jono/qr_gen/controller/QRGenController.java
@@ -5,11 +5,14 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 
+
 import static lt.jono.qr_gen.generator.GenerateQRCodes.generateQRCodes;
 
 public class QRGenController {
 
     private int selectedResolution;
+
+    private final int defaultResolution = 60;
 
     @FXML
     private TextField seriesField;
@@ -26,7 +29,7 @@ public class QRGenController {
 
     @FXML
     private void handleExitButton() {
-        Platform.exit(); // Uždarome programą
+        Platform.exit();
     }
 
 
@@ -34,31 +37,45 @@ public class QRGenController {
     private void handleResolutionMenuItemClicked(ActionEvent event) {
         MenuItem menuItem = ((MenuItem) event.getTarget());
         int resolution = Integer.parseInt(menuItem.getUserData().toString().trim());
-        System.out.println("Pasirinkta rezoliucija: " + resolution + " pix");
+        selectedResolution = (selectedResolution == 0) ? defaultResolution : selectedResolution;
         resolutionMenuButton.setText("Pasirinkta rezoliucija: " + resolution + " pix");
         selectedResolution = resolution;
-        //generateButtonClicked(); // Perduodama rezoliucija
     }
 
     @FXML
     private void generateButtonClicked() {
         String series = seriesField.getText();
-        int startNumber = Integer.parseInt(fromField.getText());
-        int endNumber = Integer.parseInt(toField.getText());
-        int resolution = selectedResolution;
-        generateQRCodes(series, startNumber, endNumber, resolution);
-        showQRGeneratedDialog();
+        int startNumber;
+        int endNumber;
+        try {
+            startNumber = Integer.parseInt(fromField.getText());
+            endNumber = Integer.parseInt(toField.getText());
+            int resolution = selectedResolution;
+            if (startNumber > endNumber) {
+                System.out.println("Klaida: pradžios skaičius negali būti didesnis už pabaigos skaičiu");
+            } else if (isValidNumber(startNumber) && isValidNumber(endNumber)) {
+                generateQRCodes(series, startNumber, endNumber, resolution);
+                showQRGeneratedDialog();
+            } else {
+                System.out.println("Klaida: įvedėte netinkamus skaičius");
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("!!Klaida: įvedėte ne skaičių!!");
+        }
     }
+
     @FXML
     private void showQRGeneratedDialog() {
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setTitle("QR Kodai Sugeneruoti");
         alert.setHeaderText(null);
         alert.setContentText("QR kodai buvo sėkmingai sugeneruoti!");
-
         ButtonType okButton = new ButtonType("Gerai");
         alert.getButtonTypes().setAll(okButton);
-
         alert.showAndWait();
+    }
+
+    public boolean isValidNumber(int number) {
+        return number > 0;
     }
 }
